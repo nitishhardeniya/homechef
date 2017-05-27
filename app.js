673de30b408e9f95e4ec855f0b71ecec6afd0582
Nitish <Nitish>
2017-05-27 17:22:23 +0530

    homechef6

--- app.js
+++ app.js
@@ -4,30 +4,34 @@ playApp.controller('playCtrl',['$scope',function($scope){
     
     $scope.messageCtrl = "Im from conrtroller";
     $scope.name = 'Nitish Hardeniya';
-    $scope.photos = [{
+    $scope.items = [{
         imgId:1,
         url:'https://www.skymetweather.com/themes/skymet/images/gallery/toplists/Top-Not-to-miss-food-items-in-Monsoon/5.jpg',
         title:'Momos'
     },{
         imgId:2,
-        url:'https://s-media-cache-ak0.pinimg.com/736x/26/d6/ce/26d6ceba8b1e558a6cc0c8f4cee6cd4f.jpg',
-        title:'Noodles'
+        url:'http://www.indobase.com/recipes/rec-images/aloo-paratha.jpg',
+        title:'Aloo paratha'
     },{
         imgId:3,
         url:'http://img3.rnkr-static.com/list_img_v2/4776/2504776/full/new-fast-food-items-2017.jpg',
         title:'Falafel'
     },{
         imgId:4,
-        url:'https://s-media-cache-ak0.pinimg.com/736x/26/d6/ce/26d6ceba8b1e558a6cc0c8f4cee6cd4f.jpg',
-        title:'Noodles'
+        url:'http://st1.thehealthsite.com/wp-content/uploads/2016/02/diet-high-calorie-count-of-indian-veg-food-chhole-bhature-THS.jpg',
+        title:'Chole bhature'
     },{
         imgId:5,
-        url:'http://img3.rnkr-static.com/list_img_v2/4776/2504776/full/new-fast-food-items-2017.jpg',
-        title:'Falafel'
+        url:'http://www.manjulaskitchen.com/blog/wp-content/uploads/everyday-lunch-menu.jpg',
+        title:'Kadi pakoda'
     },{
         imgId:6,
-        url:'https://s-media-cache-ak0.pinimg.com/736x/26/d6/ce/26d6ceba8b1e558a6cc0c8f4cee6cd4f.jpg',
-        title:'Noodles'
+        url:'http://www.onegreenplanet.org/wp-content/uploads/2010/10/2015/05/channa-1200x800.jpg',
+        title:'Chole chawal'
+    },{
+        imgId:7,
+        url:'http://foodpunch.com/wp-content/uploads/2013/07/Vegetable-Pulav-0.jpg',
+        title:'Vegetable pulav'
     }];
 
     $scope.basket = [];
